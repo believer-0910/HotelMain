700eb3bc28d1184a48121334621ab40904144708
skambarov <saidaminqambarov408@gmail.com>
2022-05-05 10:06:15 +0300

    Initial commit

---
+++ src/main/java/com/exadel/demo/HotelMainApplication.java
@@ -0,0 +1,13 @@
+package com.exadel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelMainApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exadel/demo/HotelMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exadel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
